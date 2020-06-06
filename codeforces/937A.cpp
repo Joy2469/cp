14a7778dea56bd8874c085862b5ac4e79b0bdebd
Aditi Jain <45466137+Joy2469@users.noreply.github.com>
2020-06-06 08:33:12 -0500

    Add files via upload
    https://codeforces.com/problemset/problem/937/A

---
+++ codeforces/937A.cpp
@@ -0,0 +1,44 @@
+//Aditi Jain
+//
+//https://codeforces.com/problemset/problem/937/A
+
+
+#include <iostream>
+#include <set>
+#include <vector> //include vector
+#include <algorithm>
+
+using namespace std;
+
+
+void codeforces937A()
+{
+
+
+    int input; int num;
+    cin >> num;
+
+    //adding integers in the form of
+    set <int> K;
+
+    //To add sets. by substracting we reduce one element at a time to zero. if there are different elemnts. if there is two same they become zero at the same time.
+    //Sets doesnt have repitions. so the size of a set is the number of steps we need for the array explode.
+    int i = 0;
+    while (i < num)
+    {
+        cin >> input;
+        if(input)
+        K.insert(input);
+        i++;
+    }
+
+
+
+    cout << K.size() << endl;
+
+}
+
+int main() {
+
+    codeforces937A();
+}
